15b8bc9aa5243d4d8b5eecbff60a7d89850191a2
Yaroslav <yorimark@abv.bg>
2019-05-22 13:46:18 +0300

    return to correct username and wrong db type

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ provider "aws" {
 resource "aws_db_instance" "default" {
   allocated_storage   = 20
   storage_type        = "gp2"
-  engine              = "mysql"
+  engine              = "mysqlddddddd"
   engine_version      = "5.7"
   instance_class      = "db.t2.micro"
   name                = "mydbtest"
-  username            = "-#atans-d%4"
+  username            = "atanas"
   password            = "${var.psw}"
   skip_final_snapshot = true
 }
