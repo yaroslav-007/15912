22de37feb6d250978be4cb028ee72921afe664bf
Yaroslav <yorimark@abv.bg>
2019-05-22 10:48:09 +0300

    first upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "eu-central-1"
+
+  #  access_key = ""
+  #  secret_key = ""
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage   = 20
+  storage_type        = "gp2"
+  engine              = "mysql"
+  engine_version      = "5.7"
+  instance_class      = "db.t2.micro"
+  name                = "mydbtest"
+  username            = "atans"
+  password            = ""
+  skip_final_snapshot = true
+}
